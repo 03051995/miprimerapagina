05929087402993b89372ee5a11f621e5d24e0dda
03051995 <rojasrodrigo51@gmail.com>
2023-05-31 21:50:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAlert() { 
+    alert("¡Hola, esta es una alerta desde javascript!")
+}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
